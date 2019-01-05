280799b849fa34958f2adb9371a8e82c0e555236
vesche <austinjackson892@gmail.com>
2019-01-04 22:51:28 -0700

    precommit

--- juc2/stage.py
+++ juc2/stage.py
@@ -6,12 +6,52 @@ import time
 from juc2 import art
 
 
+def _tick(FPS):
+    """Sleep desired frames per second."""
+    time.sleep(1/FPS)
+
+
+def _clear_terminal():
+    """Cross-platform terminal clear."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+    # print("%c[2J%c[;H" % (27, 27))
+
+
 class Stage:
+    """The juc2 Stage"""
+
     def __init__(self, width=40, height=20, frame=False):
         self.width = width
         self.height = height
         self.frame = frame
         self.grid = [list(' '*width) for i in range(height)]
+
+    @staticmethod
+    def prep_figure(figure):
+        """Remove blank lines from figure."""
+        return '\n'.join([row for row in figure.split('\n') if row != ''])
+
+    def display(self):
+        """Displays the grid to the terminal."""
+        for row in self.grid:
+            print(''.join(row))
+
+    def stage_eraser(self):
+        """Get the stage eraser."""
+        return art.Eraser(width=self.width, height=self.height)
+
+    def stage_frame(self):
+        """Get the stage frame."""
+        return art.Rectangle(width=self.width, height=self.height,
+                             transparent=True)
+
+    def clear_stage(self):
+        """Clear the stage."""
+        self.load(self.stage_eraser())
+
+    def load_frame(self):
+        """Load the frame."""
+        self.load(self.stage_frame())
 
     def load(self, figure):
         """Loads a figure onto the grid."""
@@ -33,7 +73,7 @@ class Stage:
     def draw(self, art_queue=None, FPS=None):
         """Draws a figure or figures onto the terminal."""
         # 1 - Clear Terminal
-        self.clear_terminal()
+        clear_terminal()
 
         # 2 - Clear Stage
         self.clear_stage()
@@ -54,41 +94,4 @@ class Stage:
 
         # 5 - Wait
         if FPS:
-            self.tick(FPS)
-
-    def display(self):
-        """Displays the grid to the terminal."""
-        for row in self.grid:
-            print(''.join(row))
-
-    @staticmethod
-    def prep_figure(figure):
-        """Remove blank lines from figure."""
-        return '\n'.join([row for row in figure.split('\n') if row != ''])
-
-    @staticmethod
-    def tick(FPS):
-        """Sleep desired frames per second."""
-        time.sleep(1/FPS)
-
-    @staticmethod
-    def clear_terminal():
-        """Cross-platform terminal clear."""
-        os.system('cls' if os.name == 'nt' else 'clear')
-        # print("%c[2J%c[;H" % (27, 27))
-
-    def clear_stage(self):
-        """Clear the stage."""
-        self.load(self.stage_eraser())
-
-    def load_frame(self):
-        """Load the frame."""
-        self.load(self.stage_frame())
-
-    def stage_eraser(self):
-        """Get the stage eraser."""
-        return art.Eraser(width=self.width, height=self.height)
-
-    def stage_frame(self):
-        """Get the stage frame."""
-        return art.Rectangle(width=self.width, height=self.height, transparent=True)
+            tick(FPS)
